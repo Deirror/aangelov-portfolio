2ffeff0cdb006d90e5a7b2c89e263380ed2808af
Deirror <alexmailbg@gmail.com>
2025-02-03 22:46:04 +0200

    add edu

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // import Image from "next/image";
 import Home from '../components/Home';
+import Education from '../components/Education';
 
 function App() {
   return (
     <main>
       <Home />
+      <Education />
     </main>
   );
 };
