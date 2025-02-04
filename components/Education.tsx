9e354d7d5a2835dfe54bcb05cb0aad81df5ab4f6
Deirror <alexmailbg@gmail.com>
2025-02-04 16:21:11 +0200

    --

--- components/Education.tsx
+++ components/Education.tsx
@@ -5,10 +5,9 @@ const Education = () => {
     return (
         <div className={styles.educationContainer}>
 
-            <div>
-            <div className={styles.line}></div> 
+           
             <div className={styles.title}>Education</div>
-            </div>
+
 
             <div className={styles.education}>
                 <div className={styles.educationCard}>
