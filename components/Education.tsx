469c7e8819ec2974b29a34f14dee386ea39b17a7
Deirror <alexmailbg@gmail.com>
2025-02-04 17:38:07 +0200

    check lines

--- components/Education.tsx
+++ components/Education.tsx
@@ -6,6 +6,8 @@ const Education = () => {
         <div className={styles.educationContainer}>
 
             <div className={styles.line}></div>
+            <div className={styles.line}></div>
+            <div className={styles.line}></div>
             <div className={styles.title}>Education</div>
             <div className={styles.line}></div>
 
