1f3bc00cfcb8e2456f074d2943099e5f1649048f
Deirror <alexmailbg@gmail.com>
2025-02-04 18:49:24 +0200

    check h3

--- components/Certification.tsx
+++ components/Certification.tsx
@@ -47,9 +47,9 @@ const Certification = () => {
                         </div>
                         <div className="timeline-marker"></div>
                         <div className="timeline-content">
-                            <h3 className="timeline-title">Mathematics | Sofia University &quot;St. Kliment Ohridski&quot;, Faculty of Mathematics and Informatics</h3>
+                            <h3 className="timeline-title">Sofia University &quot;St. Kliment Ohridski&quot;, Faculty of Mathematics and Informatics</h3>
                             <div>
-                                Attained deep understanding on complex fields
+                                Achieved  deep understanding on complex fields
                                 of Mathematics and Programming<br />
                                 Thanks to the perfect guidance by the
                                 associate lector Angel Dimitriev and the assistents
@@ -65,7 +65,7 @@ const Certification = () => {
                         <div className="timeline-info"></div>
                         <div className="timeline-marker"></div>
                         <div className="timeline-content">
-                            <h2 className="timeline-title">2022</h2>
+                            <h2 className={styles.title}>2022</h2>
                         </div>
                     </li>
                     <li className="timeline-item">
