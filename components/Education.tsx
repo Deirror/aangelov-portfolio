90d9acffc03fd77e4514b99eb48b9f50202bd51c
Deirror <alexmailbg@gmail.com>
2025-02-04 16:14:44 +0200

    add final line change

--- components/Education.tsx
+++ components/Education.tsx
@@ -5,8 +5,8 @@ const Education = () => {
     return (
         <div className={styles.educationContainer}>
 
-            <div className={styles.title}>Education</div>
             <div className={styles.line}></div>
+            <div className={styles.title}>Education</div>
 
             <div className={styles.education}>
                 <div className={styles.educationCard}>
