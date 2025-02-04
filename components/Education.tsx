0c39f67b79147daa3c6da9bd10b971cd3852a70f
Deirror <alexmailbg@gmail.com>
2025-02-04 11:14:45 +0200

    add elem school div

--- components/Education.tsx
+++ components/Education.tsx
@@ -10,20 +10,29 @@ const Education = () => {
                 <div className={styles.educationCard}>
                     <div className={styles.info}>
                         <Link className={styles.link} href="https://www.uni-sofia.bg/index.php/bul" target="_blank">
-                            <div className={styles.institutionTitle}>Sofia University St. Kliment Ohridski</div>
+                            <div className={styles.institutionTitle}>Sofia University &quot;St. Kliment Ohridski&quot;</div>
                         </Link>
-                        <div className={styles.materialTitle}>BSc Software Engineering | Faculty of Mathematics and Informatics</div>
+                        <div className={styles.materialTitle}>B.Sc. Software Engineering | Faculty of Mathematics and Informatics</div>
                     </div>
-                    <div className={styles.period}>October 2023 - Present</div>
+                    <div className={styles.period}>2023 - 2027</div>
                 </div>
                 <div className={styles.educationCard}>
                     <div className={styles.info}>
-                        <Link className={styles.link} href="https://ivanvazov.com/" target="_blank">
-                            <div className={styles.institutionTitle}>31 High School</div>
+                        <Link className={styles.link} href="http://ppmgkk.com/" target="_blank">
+                            <div className={styles.institutionTitle}>PPMG &quot;Nikola Obreshkov&quot;</div>
                         </Link>
-                        <div className={styles.materialTitle}>Mathematics & Physics</div>
+                        <div className={styles.materialTitle}>Mathematics, Informatics and IT</div>
                     </div>
                     <div className={styles.period}>2018 - 2023</div>
+                </div>
+                <div className={styles.educationCard}>
+                    <div className={styles.info}>
+                        <Link className={styles.link} href="https://oumatibolgaria.eu/" target="_blank">
+                            <div className={styles.institutionTitle}>Elementary school &quot;Mati Bolgaria&quot;</div>
+                        </Link>
+                        <div className={styles.materialTitle}>Mathematics</div>
+                    </div>
+                    <div className={styles.period}>2015 - 2018</div>
                 </div>
             </div>
         </div>
